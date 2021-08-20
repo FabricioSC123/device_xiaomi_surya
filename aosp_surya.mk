e158305abb7c8c5cf6d9dfdb25aab9e1699addd8
facoco <facoco@gmx.net>
2021-08-20 01:44:17 -0500

    AEX BringUp

--- aosp_surya.mk
+++ aosp_surya.mk
@@ -21,13 +21,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common aosp stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
-PRODUCT_NAME := lineage_surya
+PRODUCT_NAME := aosp_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3 NFC
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+#AEX stuff
+TARGET_BOOT_ANIMATION_RES := 2280
+ARGET_FACE_UNLOCK_SUPPORTED := true
+
