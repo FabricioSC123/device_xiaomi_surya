bd037012740615225527965f06003d135d6afb4d
FabricioSC123 <89610142+FabricioSC123@users.noreply.github.com>
2021-09-19 08:53:47 -0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,8 +15,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lighthouse_surya.mk
+    $(LOCAL_DIR)/radiant_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    lighthouse_surya-userdebug
+    radiant_surya-userdebug
   
